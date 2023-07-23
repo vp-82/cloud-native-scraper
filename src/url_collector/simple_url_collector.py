d1a5909dfe1e177860c7c3ad7a420e18cb92544a
vp-82 <github@pechtor.ch>
2023-07-23 16:59:13 +0200

    test commit

--- src/url_collector/simple_url_collector.py
+++ src/url_collector/simple_url_collector.py
@@ -22,3 +22,22 @@ class SimpleURLCollector(BaseURLCollector):
 
     def collect(self):
         """ Collects the base URLs from the given source. """
+        all_urls = []
+        for base_url in self.urls:
+            if base_url in self.state:
+                continue
+
+            response = requests.get(base_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            urls_from_page = self.extract_urls(soup)
+
+            # Filter URLs to only include those that start with the base_url
+            urls_from_page = [url for url in urls_from_page if url.startswith(base_url)]
+            
+            all_urls.extend(urls_from_page)
+
+            self.state[base_url] = True
+            self.save_state()
+            self.publisher_adapter.publish(base_url)
+
+        return all_urls
